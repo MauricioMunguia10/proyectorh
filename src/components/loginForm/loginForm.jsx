580c1dcfafefaf309a36db8dcec320955066e509
Davidjaciel14 <22302112@utfv.edu.mx>
2024-06-06 16:13:40 -0600

    logins y forms

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { FaRegUser } from "react-icons/fa";
+import { IoKeyOutline } from "react-icons/io5";
 import { login } from "@/lib/action";
 import styles from "./loginForm.module.css";
 import { useFormState } from "react-dom";
@@ -10,9 +12,11 @@ const LoginForm = () => {
 
   return (
     <form className={styles.form} action={formAction}>
-      <input type="text" placeholder="username" name="username" />
-      <input type="password" placeholder="password" name="password" />
-      <button>Login</button>
+      <h1>INICIAR SESIÓN</h1>
+      <div><input type="text" placeholder="username" name="username" /><FaRegUser /></div>
+      <div><input type="password" placeholder="password" name="password" ></input><IoKeyOutline /></div>
+  
+      <button className={styles.bbtn}>Login</button>
       {state?.error}
       <Link href="/register">
         {"Don't have an account?"} <b>Register</b>
@@ -22,3 +26,4 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
+
--- src/components/registerForm/registerForm.jsx
+++ src/components/registerForm/registerForm.jsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { FaRegUser } from "react-icons/fa";
-import { IoKeyOutline } from "react-icons/io5";
 import { register } from "@/lib/action";
 import styles from "./registerForm.module.css";
 import { useFormState } from "react-dom";
@@ -20,16 +18,20 @@ const RegisterForm = () => {
 
   return (
     <form className={styles.form} action={formAction}>
-    <h1>INICIAR SESIÓN</h1>
-    <div><input type="text" placeholder="username" name="username" /><FaRegUser /></div>
-    <div><input type="password" placeholder="password" name="password" ></input><IoKeyOutline /></div>
-
-    <button className={styles.bbtn}>Login</button>
-    {state?.error}
-    <Link href="/register">
-      {"Don't have an account?"} <b>Register</b>
-    </Link>
-  </form>
+      <input type="text" placeholder="username" name="username" />
+      <input type="email" placeholder="email" name="email" />
+      <input type="password" placeholder="password" name="password" />
+      <input
+        type="password"
+        placeholder="password again"
+        name="passwordRepeat"
+      />
+      <button>Register</button>
+      {state?.error}
+      <Link href="/login">
+        Have an account? <b>Login</b>
+      </Link>
+    </form>
   );
 };
 
