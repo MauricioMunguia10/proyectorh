a6d05330f805aa32cbf693d1d2414b9d99c25b81
Davidjaciel14 <22302112@utfv.edu.mx>
2024-06-06 15:57:19 -0600

    el bueno

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,15 +1,20 @@
 import LoginForm from "@/components/loginForm/loginForm";
-import { handleGithubLogin } from "@/lib/action";
+import Image from "next/image";
 import styles from "./login.module.css";
 
 const LoginPage = () => {
 
   return (
+  
     <div className={styles.container}>
+     
+      <div className={styles.menu}>
+      <Image className={styles.fondo} src="/3.png" alt="" width={700} height={800}></Image>
+         <Image className={styles.img2} src="/2.png" alt="" width={500} height={700}></Image>
+         <Image className={styles.img} src="/pc-login.png" alt="" width={400} height={400}></Image>
+      </div>
       <div className={styles.wrapper}>
-        <form action={handleGithubLogin}>
-          <button className={styles.github}>Login with Github</button>
-        </form>
+        
         <LoginForm />
       </div>
     </div>
