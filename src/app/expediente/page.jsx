ac547dc94575fed9427d9f6cf1b212a729543f7c
Mauricio Munguia <mauriciomunguiagarcia@gmail.com>
2024-06-07 11:58:53 -0600

    add layout interno

--- src/app/expediente/page.jsx
+++ src/app/expediente/page.jsx
@@ -1,44 +1,48 @@
-"use client"
-import styles from './expediente.module.css'
-import Card from '../../components/card/card';
-import ModalExpediente from '@/components/modalExpediente/modal';
-import PersonIcon from '@mui/icons-material/Person';
-import { useState } from 'react';
-import { useEffect } from 'react';
+"use client";
+import styles from "./expediente.module.css";
+import Card from "../../components/card/card";
+import ModalExpediente from "@/components/modalExpediente/modal";
+import PersonIcon from "@mui/icons-material/Person";
+import { useState } from "react";
+import { useEffect } from "react";
+import LayoutInterno from "@/components/layoutInterno/LayoutInterno";
 
 const Expediente = () => {
-  const [visible, setVisible] = useState (true)
-  const [nombre, setNombre] = useState ("user")
-  const mostrarModal = (name) =>{
+  const [visible, setVisible] = useState(true);
+  const [nombre, setNombre] = useState("user");
+  const mostrarModal = (name) => {
     setVisible(!visible);
-    setNombre(name)
-    console.log("Entra")
-  }
+    setNombre(name);
+    console.log("Entra");
+  };
 
-    return (
+  return (
+    <LayoutInterno>
       <div className={styles.container}>
         <h2>Expedinete Empleados</h2>
-        
+
         <div className={visible ? styles.modal : styles.modalInvisible}>
           <div className={styles.side}>
-            <PersonIcon/>
+            <PersonIcon />
             <h1>{nombre}</h1>
-          
           </div>
-          
-          
         </div>
-        
-        <div className={styles.containerExpedientes}>
-        
-        <Card nombre="Juan Diaz" puesto="Desarrollador" onClick={()=>mostrarModal("Juan Diaz")} />
-        <Card nombre="Mauricio M." puesto="Desarrollador" onClick={()=>mostrarModal("Mauricio Munguia")} />
 
+        <div className={styles.containerExpedientes}>
+          <Card
+            nombre="Juan Diaz"
+            puesto="Desarrollador"
+            onClick={() => mostrarModal("Juan Diaz")}
+          />
+          <Card
+            nombre="Mauricio M."
+            puesto="Desarrollador"
+            onClick={() => mostrarModal("Mauricio Munguia")}
+          />
         </div>
-       
-        </div>
-   
-    );
-  };
-  
-  export default Expediente;
+      </div>
+    </LayoutInterno>
+  );
+};
+
+export default Expediente;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,37 +1,20 @@
-
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar/Navbar";
-import Footer from "@/components/footer/Footer";
-import ClientSideProviderTest from "@/components/clientSideProviderTest";
-import SideBar from "@/components/sideBar/sideBar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: {
-    default:"Next.js 14 Homepage",
-    template:"%s | Next.js 14"
+    default: "Next.js 14 Homepage",
+    template: "%s | Next.js 14",
   },
   description: "Next.js starter app description",
 };
 
-
 export default function RootLayout({ children }) {
- 
- 
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <ClientSideProviderTest> */}
-          <div className="container">
-          <SideBar/> 
-            <Navbar/>
-            
-
-            {children}
-      
-          </div>
-        {/* </ClientSideProviderTest> */}
+        <div className="container">{children}</div>
       </body>
     </html>
   );
